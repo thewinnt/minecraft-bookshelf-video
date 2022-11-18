debd6bb76b32a0a89ce514b34fef5832cefb7378
WinNT <25638225+thewinnt@users.noreply.github.com>
2022-11-18 17:52:16 +0300

    Make it a documented datapack generator

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# bad-apple-bookshelf
- Bad Apple with minecraft's chiseled bookshelves
+# Bad Apple
+A script to make Bad Apple (and other videos) with Minecraft's chiseled bookshelves
+
+# Requirements
+- [ffmpeg](https://ffmpeg.org/download.html) (or any other tool to convert a video into individual frames)
+- at least 100 MB of storage (or more for long videos)
+- at least 2-3 GB of RAM to allocate to Minecraft (long videos require more RAM, 2 GB will not be enough in that case)
+- a video in 480x360 resolution
+- Python 3.6 or newer with PIL (pillow) installed
+- Minecraft: Java Edition 22w46a+
+
+# Usage
+- Install Pillow with the command `pip install pillow` (in a terminal) if you haven't already
+- Put the frames of your video in the `frames` folder (create it first). If you're using ffmpeg:
+  - Run `<ffmpeg> -i <your video> -r <desired framerate> <path to script>/frames/%d.jpg`, where:
+    - `ffmpeg` is the path to your ffmpeg installation, if it's not installed globally
+    - `your video` is the path to the source video
+    - `desired framerate` makes ffmpeg skip some frames, giving you only so much frames per second. Can be used to adjust the speed of the video. With the default settings, higher values make the video slower, lower values make it faster. 20 gives the normal speed.
+    - `path to script` is the path to the folder where you put the script
+    - The rest of the command should not be changed
+  - Adjust the settings in the script. They're the values after the words in all caps:
+    - `FRAME_COUNT` is how much frames you got after converting, or something smaller (in which case the video will cut off early)
+    - `STEP_SKIP` is how much frames to step over when generating the datapack. Use this to adjust the framerate of the video. The default value gives 20 FPS, the maximum possible.
+    - `SPEED` is the speed of the video. Use this to slow down or speed up the video together with the `desired framerate` used before. The default value gives you the speed of (20/`desired framerate`) x normal speed.
+    - All of these should be whole numbers and at least 1.
+  - Run the script and wait until it finishes.
+  - Add the generated datapack to your world upon creation (or later, if you've enabled the 1.20 features before) and enable cheats
+  - Run `/function video:start` to play the video. It will appear around the coordinates 20 110 0
+  - Use `/function video:stop` to stop the video.
+  - Enjoy!
--- script.py
+++ script.py
@@ -1,16 +1,29 @@
 from PIL import Image
+import os
+
+# =====================
+# SETTINGS
+FRAME_COUNT = 4384  # The amount of frames in your video. Must not be larger than what you have in the frames folder.
+STEP_SKIP = 1  # Makes the framerate 20/STEP by skipping some frames. Higher value = lower video FPS = lower file size and system requirements
+SPEED = 1  # Adjusts the speed of the video. Higher value = slower video
+# ^ these two above add together resulting if a framerate of (20 / STEP_SKIP) / STEP_TIME
+# =====================
 
 commands = []
 dispatch_cmd = []
 
-for i in range(1, 4384, 4):
-    if len(str(i)) == 1:
-        name = "00" + str(i)
-    elif len(str(i)) == 2:
-        name = "0" + str(i)
-    else:
-        name = i
-    img = Image.open(f"frames/{name}.jpg")
+try:
+    os.makedirs("generated_datapack/data/minecraft/tags/functions")
+except FileExistsError:
+    pass
+try:
+    os.makedirs("generated_datapack/data/video/functions")
+except FileExistsError:
+    pass
+
+for i in range(1, FRAME_COUNT, STEP_SKIP):
+    img = Image.open(f"frames/{i}.jpg")
+    # Feel free to play around with the numbers
     for x in range(40):
         for y in range(22):
             c1 = str(sum(img.getpixel((479 - (x * 12 + 8), 359 - (y * 16 + 8)))) > 375).lower() # approx
@@ -20,9 +33,38 @@ for i in range(1, 4384, 4):
             c5 = str(sum(img.getpixel((479 - (x * 12 + 4), 359 - (y * 16)))) > 375).lower()
             c6 = str(sum(img.getpixel((479 - (x * 12), 359 - (y * 16)))) > 375).lower()
             commands.append(f"setblock {x} {100 + y} 0 chiseled_bookshelf[slot_0_occupied={c1},slot_1_occupied={c2},slot_2_occupied={c3},slot_3_occupied={c4},slot_4_occupied={c5},slot_5_occupied={c6}]\n")
-    with open(f"output/{i}.mcfunction", "w+") as file:
+    with open(f"generated_datapack/data/video/functions/{i}.mcfunction", "w") as file:
         file.writelines(commands)
     commands = []
-    dispatch_cmd.append(f"execute if score tick badapple matches {i} run function minecraft:output/{i}\n")
-with open("dispatch.mcfunction", "w+") as file:
+    dispatch_cmd.append(f"execute if score tick video matches {i*SPEED} run function video:{i}\n")
+    print("Finished frame", i, "/", FRAME_COUNT)
+
+with open("generated_datapack/data/video/functions/dispatch.mcfunction", "w") as file:
+    dispatch_cmd.append("execute if score tick video matches 1.. run scoreboard players add tick video 1\n")
     file.writelines(dispatch_cmd)
+
+with open("generated_datapack/data/video/functions/load.mcfunction", "w") as file:
+    file.writelines([
+        "scoreboard objectives add video dummy\n",
+        "scoreboard players set tick video 0"
+    ])
+
+with open("generated_datapack/data/video/functions/start.mcfunction", "w") as file:
+    file.writelines([
+        "scoreboard players set tick video 1"
+    ])
+
+with open("generated_datapack/data/video/functions/stop.mcfunction", "w") as file:
+    file.writelines([
+        "scoreboard players set tick video 0\n",
+        "fill 0 100 0 39 121 0 chiseled_bookshelf"
+    ])
+
+with open("generated_datapack/data/minecraft/tags/functions/load.json", "w") as file:
+    file.write('{"values": ["video:load"]}')
+
+with open("generated_datapack/data/minecraft/tags/functions/tick.json", "w") as file:
+    file.write('{"values": ["video:dispatch"]}')
+
+with open("generated_datapack/pack.mcmeta", "w") as file:
+    file.write('{"pack": {"pack_format": 10, "description": "Plays a video with chiseled bookshelves"}, "features": {"enabled": ["update_1_20"]}}')
